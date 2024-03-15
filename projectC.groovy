6e10a8a504e0f4bc95727b428b06f75917c3697f
umairshah124 <63007589+umairshah124@users.noreply.github.com>
2024-03-15 10:25:21 +0500

    Update projectC.groovy

--- projectC.groovy
+++ projectC.groovy
@@ -1,23 +1,18 @@
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                // Your build steps for project B here
-                sh 'echo "Building Project C"'
+pipelineJobNames = ['projectA', 'projectB']
+pipelineJobNames.each { jobName ->
+    pipelineJob(jobName) {
+      definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/umairshah124/seed_job.git')
+                    }
+                    branches('*/main')
+                }
             }
-        }
-        stage('Test') {
-            steps {
-                // Your test steps for project B here
-                sh 'echo "Testing Project C"'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                // Your deployment steps for project B here
-                sh 'echo "Deploying Project C"'
-            }
+          scriptPath("${jobName}.groovy") 
         }
     }
+  }
 }
